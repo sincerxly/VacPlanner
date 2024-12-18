c6b77145f894a90edea9a4b9d0df14623fe38850
bbqcream <imground11@gmail.com>
2024-12-18 19:37:06 +0900

    feat :: 사이드바 추가

--- src/pages/main/elements/sideBar/index.tsx
+++ src/pages/main/elements/sideBar/index.tsx
@@ -3,6 +3,8 @@ import * as S from "./style";
 
 const SideBar = () => {
   const [isClose, setIsClose] = useState<boolean>(false);
+  const [bgColor, setBgColor] = useState<string>("#000000");
+  const [name, setName] = useState<string>();
 
   return (
     <S.Container isClose={isClose}>
@@ -12,7 +14,36 @@ const SideBar = () => {
         isClose={isClose}
       />
       <S.FuncWrapper>
-        <S.FuncContainer></S.FuncContainer>
+        <S.FuncContainer isClose={isClose}>
+          <S.Func>
+            배경색 :
+            <S.ChooseColor bgColor={bgColor}>
+              <S.ColorBox
+                type="color"
+                onChange={(e) => setBgColor(e.target.value)}
+              />
+              {bgColor}
+            </S.ChooseColor>
+          </S.Func>
+          <S.Func>
+            내용 :
+            <S.DetailInput
+              placeholder="이름을 입력해주세요."
+              onChange={(e) => setName(e.target.value)}
+            />
+          </S.Func>
+          <S.TimeFunc>
+            시간 :
+            <S.TimeWrapper>
+              <S.TimeInput type="time"></S.TimeInput>-
+              <S.TimeInput type="time"></S.TimeInput>
+            </S.TimeWrapper>
+          </S.TimeFunc>
+          <S.Func>
+            사진 :<S.ImageText>이미지 업로드</S.ImageText>
+          </S.Func>
+        </S.FuncContainer>
+        <S.DeleteButton>테이블 삭제</S.DeleteButton>
       </S.FuncWrapper>
     </S.Container>
   );
--- src/pages/main/elements/sideBar/style.tsx
+++ src/pages/main/elements/sideBar/style.tsx
@@ -1,13 +1,34 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
 
 interface PushProps {
   isClose: boolean;
 }
 
+interface ColorProps {
+  bgColor: string;
+}
+
 export const Box = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: Center;
+  align-items: center;
 `;
 
 export const Container = styled.div<PushProps>`
@@ -20,6 +41,8 @@ export const Container = styled.div<PushProps>`
   box-shadow: 0px 0px 2px 0px rgb(0, 0, 0, 25%);
   transition: width 0.5s;
   transition-timing-function: ease-in-out;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Push = styled.img<PushProps>`
@@ -34,10 +57,81 @@ export const Push = styled.img<PushProps>`
 export const FuncWrapper = styled(Box)`
   flex-direction: column;
   width: 167px;
-  height: 179px;
+  height: 100%;
 `;
 
-export const FuncContainer = styled(Box)`
+export const FuncContainer = styled(Box)<PushProps>`
+  flex-direction: column;
+  margin-top: 40px;
   width: 167px;
-  height: 16px;
+  gap: 27px;
+  display: ${(props) => (props.isClose ? "none" : {})};
+  animation: ${(props) => (props.isClose ? fadeOut : fadeIn)} 1.5s;
+`;
+
+export const Func = styled(Box)`
+  width: 100%;
+  height: 24px;
+  justify-content: space-around;
+`;
+
+export const DetailInput = styled.input`
+  width: 125px;
+  height: 100%;
+  padding-left: 3%;
+  border: 0px;
+  outline: none;
+  font-size: 12px;
+  border-radius: 3px;
+  &::placeholder {
+    color: #bfbfbf;
+    font-size: 8px;
+  }
+`;
+
+export const ChooseColor = styled(Box)<ColorProps>`
+  gap: 5px;
+  color: ${(props) => props.bgColor};
+  width: 125px;
+  justify-content: flex-end;
+`;
+
+export const ColorBox = styled.input`
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  width: 25px;
+  border-radius: 100px;
+`;
+
+export const TimeFunc = styled(Func)`
+  height: 66px;
+  width: 100%;
+`;
+
+export const TimeWrapper = styled(Box)`
+  width: 125px;
+  flex-direction: column;
+`;
+
+export const TimeInput = styled.input`
+  width: 125px;
+  height: 24px;
+`;
+
+export const ImageText = styled.div`
+  width: 125px;
+  cursor: pointer;
+  color: #00589c;
+`;
+
+export const DeleteButton = styled(Box)`
+  width: 151px;
+  height: 30px;
+  border-radius: 8px;
+  color: #e84545;
+  background-color: white;
+  margin-bottom: 40px;
+  justify-content: center;
+  cursor: pointer;
 `;
