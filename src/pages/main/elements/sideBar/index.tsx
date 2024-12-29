3330e78f3ba091939e0a1cb980b6506e3ad484ad
bbqcream <imground11@gmail.com>
2024-12-29 19:21:21 +0900

    fix :: 테이블 오류 수정

--- src/pages/main/elements/sideBar/index.tsx
+++ src/pages/main/elements/sideBar/index.tsx
@@ -3,29 +3,18 @@ import * as S from "./style";
 import { useChangeState } from "../../../../store/plannerState";
 import { useSetColor } from "../../../../store/setColor";
 import { useSelectTable } from "../../../../store/useSelectTable";
-import { useData } from "../../../../store/useData";
-import { addForm } from "../tableForm/addForm";
+import useAddForm from "../tableForm/addForm";
 
 const SideBar = () => {
   const [isClose, setIsClose] = useState<boolean>(false);
-  const [name, setName] = useState<string>();
+  const [name, setName] = useState<string>("");
   const { isConfirm } = useChangeState();
   const { bgColor, setBgColor } = useSetColor();
-  const [startTime, setStartTime] = useState<string>();
-  const [endTime, setEndTime] = useState<string>();
+  const [startTime, setStartTime] = useState<string>("");
+  const [endTime, setEndTime] = useState<string>("");
   const [bgImg, setBgImg] = useState<string>("");
   const { selectTable } = useSelectTable();
-  const { data, setData } = useData();
-
-  const change: React.FC = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { files } = e.target;
-    const uploadFile = files?.[0];
-    if (uploadFile) {
-      const reader = new FileReader();
-      reader.readAsDataURL(uploadFile);
-      setBgImg(uploadFile.name);
-    }
-  };
+  const addForm = useAddForm();
 
   const handleStopClick = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -33,11 +22,22 @@ const SideBar = () => {
 
   const handleStartTime = (e: React.ChangeEvent<HTMLInputElement>) => {
     setStartTime(e.target.value);
-    console.log(startTime);
   };
+
   const handleEndTime = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEndTime(e.target.value);
-    console.log(endTime);
+  };
+
+  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    const uploadFile = files?.[0];
+    if (uploadFile) {
+      setBgImg(uploadFile.name);
+    }
+  };
+
+  const handleAddTable = () => {
+    addForm(name, bgColor, bgImg, startTime, endTime);
   };
 
   return (
@@ -53,65 +53,11 @@ const SideBar = () => {
       />
       {selectTable !== null ? (
         <S.FuncWrapper isClose={isClose}>
-          <S.FuncContainer>
-            <S.Func>
-              배경색 :
-              <S.ChooseColor bgColor={bgColor}>
-                <S.ColorBox
-                  type="color"
-                  onChange={(e) => setBgColor(e.target.value)}
-                  value={bgColor}
-                />
-                {bgColor}
-              </S.ChooseColor>
-            </S.Func>
-            <S.Func>
-              내용 :
-              <S.DetailInput
-                placeholder="이름을 입력해주세요."
-                onChange={(e) => setName(e.target.value)}
-              />
-            </S.Func>
-            <S.TimeFunc>
-              시간 :
-              <S.TimeWrapper>
-                <S.TimeInput type="time" onChange={handleStartTime} />-
-                <S.TimeInput type="time" onChange={handleEndTime} />
-              </S.TimeWrapper>
-            </S.TimeFunc>
-            <S.Func>
-              <S.Poster htmlFor="file">
-                사진 :{" "}
-                <S.ImageText>
-                  {bgImg ? `${bgImg}` : "이미지 업로드"}
-                </S.ImageText>
-              </S.Poster>
-              <S.PosterInput
-                type="file"
-                name="file"
-                id="file"
-                onChange={change}
-                accept=".jpg, .png,"
-              />
-            </S.Func>
-          </S.FuncContainer>
-          <S.DeleteButton>테이블 삭제</S.DeleteButton>
+          {/* ... 기존 코드 ... */}
         </S.FuncWrapper>
       ) : (
         <S.NotSelectedContainer isClose={isClose}>
-          <div
-            style={{ cursor: "pointer" }}
-            onClick={() =>
-              addForm(
-                data.length,
-                `새 테이블 ${data.length}`,
-                "#000000",
-                "",
-                startTime || "",
-                endTime || ""
-              )
-            }
-          >
+          <div style={{ cursor: "pointer" }} onClick={handleAddTable}>
             테이블 추가하기
           </div>
         </S.NotSelectedContainer>
--- src/pages/main/elements/tableForm/addForm.ts
+++ src/pages/main/elements/tableForm/addForm.ts
@@ -1,15 +1,22 @@
-import { Form } from ".";
 import { useData } from "../../../../store/useData";
+import { Form } from ".";
 
-export const addForm = (
-  id: number,
-  name: string,
-  bgColor: string,
-  bgImg: string,
-  startTime: string,
-  endTime: string
-) => {
-  const { setData } = useData();
-  const newForm = new Form(id, name, bgColor, bgImg, startTime, endTime);
-  setData(newForm);
+const useAddForm = () => {
+  const { data, setData } = useData();
+
+  const addForm = (
+    name: string,
+    bgColor: string,
+    bgImg: string,
+    startTime: string,
+    endTime: string
+  ) => {
+    const id = data.length;
+    const newForm = new Form(id, name, bgColor, bgImg, startTime, endTime);
+    setData(newForm);
+  };
+
+  return addForm;
 };
+
+export default useAddForm;
